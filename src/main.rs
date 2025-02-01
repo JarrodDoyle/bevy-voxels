6e77d13964d244329da719f6aaa60669b70d1659
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-02-01 18:28:43 +0000

    Add fastnoise2

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use bevy::{
     window::PresentMode,
 };
 use bevy_flycam::PlayerPlugin;
+use fastnoise2::SafeNode;
 
 #[derive(Clone, Copy, PartialEq)]
 enum BlockType {
@@ -13,11 +14,32 @@ enum BlockType {
     Air,
 }
 
-fn gen_chunk_mesh(modulo: usize) -> Mesh {
+#[derive(Resource)]
+struct WorldNoise {
+    terrain: SafeNode,
+}
+
+fn gen_chunk_mesh(x: usize, y: usize, z: usize, world_noise: &Res<WorldNoise>) -> Mesh {
     const CHUNK_LEN: usize = 32;
+    const FREQUENCY: f32 = 0.005;
+    const SEED: i32 = 1338;
+
+    let mut noise_vals = vec![0.0; CHUNK_LEN * CHUNK_LEN * CHUNK_LEN];
+    world_noise.terrain.gen_uniform_grid_3d(
+        &mut noise_vals,
+        (CHUNK_LEN * x) as i32,
+        (CHUNK_LEN * y) as i32,
+        (CHUNK_LEN * z) as i32,
+        CHUNK_LEN as i32,
+        CHUNK_LEN as i32,
+        CHUNK_LEN as i32,
+        FREQUENCY,
+        SEED,
+    );
+
     let mut voxels = [BlockType::Air; CHUNK_LEN * CHUNK_LEN * CHUNK_LEN];
     (0..CHUNK_LEN * CHUNK_LEN * CHUNK_LEN).for_each(|i| {
-        if i % modulo == 0 {
+        if noise_vals[i] > 0. {
             voxels[i] = BlockType::Stone;
         }
     });
@@ -151,6 +173,7 @@ fn setup_temp_geometry(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    noise: Res<WorldNoise>,
 ) {
     commands.spawn((
         Mesh3d(meshes.add(Circle::new(4.0))),
@@ -179,14 +202,12 @@ fn setup_temp_geometry(
         Color::srgb_u8(104, 56, 108),
     ];
 
-    let primes = [3, 5, 7, 11, 13, 17, 19, 23];
-
     let mut i = 0;
-    for z in 0..2 {
-        for y in 0..2 {
-            for x in 0..2 {
+    for z in 0..5 {
+        for y in 0..5 {
+            for x in 0..5 {
                 commands.spawn((
-                    Mesh3d(meshes.add(gen_chunk_mesh(primes[i % primes.len()]))),
+                    Mesh3d(meshes.add(gen_chunk_mesh(x, y, z, &noise))),
                     MeshMaterial3d(materials.add(colors[i % colors.len()])),
                     Transform::from_xyz(x as f32 * 32., y as f32 * 32., z as f32 * 32.),
                 ));
@@ -194,6 +215,13 @@ fn setup_temp_geometry(
             }
         }
     }
+}
+
+fn setup_noise(mut commands: Commands) {
+    let encoded_node_tree = "DQADAAAAAAAAQCkAAAAAAD8AAAAAAA==";
+    let node = SafeNode::from_encoded_node_tree(encoded_node_tree).unwrap();
+
+    commands.insert_resource(WorldNoise { terrain: node });
 }
 
 fn toggle_vsync(input: Res<ButtonInput<KeyCode>>, mut windows: Query<&mut Window>) {
@@ -227,7 +255,7 @@ fn main() {
             FrameTimeDiagnosticsPlugin,
         ))
         .add_plugins(PlayerPlugin)
-        .add_systems(Startup, setup_temp_geometry)
+        .add_systems(Startup, (setup_noise, setup_temp_geometry).chain())
         .add_systems(Update, toggle_vsync)
         .run();
 }
