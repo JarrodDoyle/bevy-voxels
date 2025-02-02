b17bcba41fe4fb6f2c0b45c5d3c554236f2584e6
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-02-02 11:36:52 +0000

    Fix incorrect texture UVs

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ fn gen_chunk_mesh(voxels: &[BlockType]) -> Option<Mesh> {
         (0., 0., 0.),
     ];
     const RAW_INDICES: [usize; 24] = [
-        0, 1, 2, 3, 4, 5, 6, 7, 1, 4, 7, 2, 5, 0, 3, 6, 5, 4, 1, 0, 3, 2, 7, 6,
+        0, 1, 2, 3, 6, 7, 4, 5, 7, 2, 1, 4, 3, 6, 5, 0, 5, 4, 1, 0, 3, 2, 7, 6,
     ];
 
     let mut vs: Vec<[f32; 3]> = vec![];
@@ -138,10 +138,10 @@ fn gen_chunk_mesh(voxels: &[BlockType]) -> Option<Mesh> {
                             ts.push(voxels[idx] as u32);
                         }
 
-                        uvs.push([1., 0.]);
-                        uvs.push([1., 1.]);
-                        uvs.push([0., 1.]);
                         uvs.push([0., 0.]);
+                        uvs.push([0., 1.]);
+                        uvs.push([1., 1.]);
+                        uvs.push([1., 0.]);
 
                         is.push(vcount);
                         is.push(vcount + 2);
